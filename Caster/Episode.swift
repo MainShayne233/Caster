ebbace3de7af110a77cf86054bd8b324d4e65d9f
Josh Koch <xxjoshxx1225@gmail.com>
2015-12-08 16:03:27 -0600

    New pods
    New pods to be merged with master

---
+++ Caster/Episode.swift
@@ -0,0 +1,17 @@
+//
+//  Episode.swift
+//  Caster
+//
+//  Created by Josh Koch on 12/8/15.
+//  Copyright © 2015 Josh Koch. All rights reserved.
+//
+
+import Foundation
+
+struct Episode {
+    let episodeTitle: String?
+    let episodeSummary: String?
+    let episodeDuration: String?
+    let episodeDate: String?
+    let episodeUrl: NSURL?
+}
